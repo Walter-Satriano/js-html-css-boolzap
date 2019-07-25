3992166dc24230ebeb123e46ccf103033ac186d2
Walter Satriano <walter.satri@gmail.com>
2019-07-25 13:53:36 +0200

    aggiunta funzione per invio messaggio tramite pulsante tastiera + aggiunta funzione che associa ogni chat al proprio utente

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 $(document).ready(function() {
 
   // FUNZIONE DI INVIO MESSAGGIO/RISPOSTA AUTOMATICA
-  $("#send_message").click(
-    function() {
+  $("#send_message").click(sendmsg);
 
-      //salvo il valore di input
-      var inputUser = $(".write_msg").val();
-      var msg = $("#template .message_green").clone();
+  function sendmsg() {
 
-      // stampo in pagina il messaggio
-      msg.text(inputUser);
-      $(".chat_room").append(msg);
+    //salvo il valore di input
+    var inputUser = $(".write_msg").val();
+    var msg = $("#template .message_green").clone();
 
-      //pulisco l'input
-      $(".write_msg").val("");
+    // stampo in pagina il messaggio
+    msg.text(inputUser);
+    $(".chat_room.active").append(msg);
 
-      //quando inserisco un messaggio, dopo 1 sec appare la risposta
-      setTimeout(function () {
-        var answer = $("#template .message_white").text("Ok").clone();
-        $(".chat_room").append(answer);
-      }, 1000);
+    //pulisco l'input
+    $(".write_msg").val("");
+
+    //quando inserisco un messaggio, dopo 1 sec appare la risposta
+    setTimeout(function() {
+      var answer = $("#template .message_white").text("Ok").clone();
+      $(".chat_room.active").append(answer);
+    }, 1000);
+  };
+
+  // invio il messaggio premendo il pulsante "invio" da tastiera
+  $(".write_msg").keypress(
+    function(invio) {
+      if (invio.which == 13) {
+        sendmsg();
+      }
     }
   );
+
 
   // FUNZIONE DI RICERCA UTENTE
   $(".chat_search input").keyup(
@@ -43,12 +53,31 @@ $(document).ready(function() {
           // se l'input è incluso nascondo gli utenti non trovati
           if (!chatUser.includes(searchInput)) {
             $(this).hide();
-          }
+          };
         }
       );
     }
   );
 
+  /* Milestone 3
+  - Click sul contatto mostra la conversazione del contatto cliccato,
+  è possibile inserire nuovi messaggi per ogni conversazione.
+  - Cancella messaggio: cliccando sul messaggio appare un menu a tendina
+  che permette di cancellare il messaggio selezionato. */
+
+  $(".chat_items").click(
+    function() {
+
+      //quando il contatto è cliccato cambia colore
+      $(this).toggleClass("bg_items");
+
+      var refchat = $(this).attr("refchat");
+
+      $(".chat_room").removeClass("active");
+
+      $(".chat_room[refchat='"+ refchat +"']").addClass("active");
+    }
+  );
 
 
 
